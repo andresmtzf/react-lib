b6d4ac70ecc1b118fb019c681f5e4a5225f2bbab
Andres Martinez <andres.martinez@habil.mx>
2023-05-22 12:49:33 -0600

    agrega models y aumenta version

---
+++ src/models/index.ts
@@ -0,0 +1,3 @@
+export { default as Notificacion } from './notificacion'
+export { default as WithChildren } from './witchChildren'
+export { default as Paginado } from './paginado'
---
+++ src/models/paginado.ts
@@ -0,0 +1,8 @@
+interface Paginado {
+  pagina: number
+  resgistrosPorPagina: number
+  totalRegistros: number
+  totalPaginas: number
+}
+
+export default Paginado
